92354dd2915c3492d9866a889e835e450de227bd
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2018-04-25 11:05:05 +0200

    Added updateOrderStatus DAO -- Make it work with Emils order class

--- src/main/java/DBAccess/DataAccessObject.java
+++ src/main/java/DBAccess/DataAccessObject.java
@@ -103,4 +103,20 @@ public class DataAccessObject {
         }
     }
 
+   
+        //Adapt emils order class XD
+        //Make it so String status is a dropdown menu with different choices for update
+        public static void updateOrderStatus(String status, int orderId) throws LoginSampleException{
+    try {
+            Connection con = Connector.connection();
+            String SQL = "UPDATE `ordre´ SET `status´=`?´ WHERE `id´=?";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setString(1, status);
+            ps.setInt(2, order.getId());
+            ps.executeUpdate();
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new LoginSampleException(ex.getMessage());
+        }
+    }
+
 }
